83c49f06038e3546abdfeeb81e06e3e64a3595c5
Tomas Leffew <tleffew1994@gmail.com>
2016-03-30 19:17:28 -0500

    adding partials

--- public/index.php
+++ public/index.php
@@ -10,6 +10,8 @@
         <title>Skatelister</title>
     </head>
     <body>
+        <?php require_once __DIR__ .'/../views/partials/navbar.php'; ?>
+        <?php require_once __DIR__ . '/../views/partials/footer.php'; ?>
         <script src="/js/jquery-1.12.0.js"></script>
         <script src="/js/main.js"></script>
     </body>
---
+++ views/partials/footer.php
@@ -0,0 +1,1 @@
+<img class="img" src="/img/site_imgs/GitHub-Mark-120px-plus.png" alt="GitHub"/>
