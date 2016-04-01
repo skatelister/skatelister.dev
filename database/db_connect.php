48913048f8f4ee5aac4d065177d62e093eec1ecf
Tomas Leffew <tleffew1994@gmail.com>
2016-04-01 12:02:34 -0500

    refactored sign in and log in page

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$dbc = new PDO('mysql:host=' . DB_host . ';dbname=' . DB_name,
-DB_user,DB_password);
+$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
+DB_USER,DB_PASSWORD);
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
--- database/migration.php
+++ database/migration.php
@@ -2,14 +2,22 @@
 require_once __DIR__ . '/../skateConfig.php';
 require_once 'db_connect.php';
 
+$itemsDropTable = "DROP TABLE IF EXISTS items";
+$dbc->exec($itemsDropTable);
+
+$userDropTable = "DROP TABLE IF EXISTS users";
+$dbc->exec($userDropTable);
+
+
+
 $userTable = "CREATE TABLE users(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   first_name  VARCHAR(40) NOT NULL ,
   last_name VARCHAR(40) NOT NULL,
   email VARCHAR(40) NOT NULL UNIQUE,
-  user_pass VARCHAR(40) NOT NULL,
+  user_pass VARCHAR(120) NOT NULL,
   PRIMARY KEY(id)
-);";
+)";
 $dbc->exec($userTable);
 
 $itemsTable = "CREATE TABLE items (
@@ -23,7 +31,7 @@ $itemsTable = "CREATE TABLE items (
   user_id INT(5) UNSIGNED NOT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY (user_id) REFERENCES users(id)
-);";
+)";
  $dbc->exec($itemsTable);
 
 ?>
