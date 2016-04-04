fe7546351cb60e427f757d22c2c8fa31846eab20
CJ Sampson <cjsampson41@gmail.com>
2016-04-04 16:47:01 -0500

    worked on the styling of the page

--- public/index.php
+++ public/index.php
@@ -13,16 +13,5 @@ var_dump($newestAds);
 
 <?php require_once '../views/partials/header.php'; ?>
 
-<<<<<<< HEAD
-=======
-<?php
-
-$newestAds = Ad::showNewest();
-
-var_dump($newestAds);
-?>
->>>>>>> e53bd24801e659afd8d89a4fcaefe65082a6d321
-
-
 
 <?php require_once '../views/partials/footer.php'; ?>
