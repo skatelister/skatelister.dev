005fedaa7ec4412b3fbd087b092ce054ada3a569
CJ Sampson <cjsampson41@gmail.com>
2016-04-05 20:05:17 -0500

    Fixed spelling error LOL!

--- controllers/signin.php
+++ controllers/signin.php
@@ -1,25 +1,27 @@
-<main>
-    <form method="post">
-
-      <div class="">
-        <label for="email">Email:</label>
-        <input id="email" type="text" name="email" value="<?=  Input::get('email'); ?>">
-          <span id='errors_email'></span>
-      </div>
-      <div class="">
-        <label for="password">Password:</label>
-        <input id="password" type="text" name="password" value="">
-        <span id="errors_password"></span>
-
-      </div>
 
 
-      <span id="errors_wronginfo"></span>
-      <button id="login_button"type="submit" name="button">submit</button>
-    </form>
-    <form class="" method="post">
-      <div class="">
-        <button type="submit" name="sign_up"> Sign up</button>
-      </div>
-    </form>
-</main>
+  <main>
+      <form method="post">
+
+        <div class="">
+          <label for="email">Email:</label>
+          <input id="email" type="text" name="email" value="<?=  Input::get('email'); ?>">
+            <span id='errors_email'></span>
+        </div>
+        <div class="">
+          <label for="password">Password:</label>
+          <input id="password" type="text" name="password" value="">
+          <span id="errors_password"></span>
+
+        </div>
+
+
+        <span id="errors_wronginfo"></span>
+        <button id="login_button"type="submit" name="button">submit</button>
+      </form>
+      <form class="" method="post">
+        <div class="">
+          <button type="submit" name="sign_up"> Sign up</button>
+        </div>
+      </form>
+  </main>
