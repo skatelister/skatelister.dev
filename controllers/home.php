7a916e159cef5439b9125432d6d03f34a6e89588
Tomas Leffew <tleffew1994@gmail.com>
2016-04-07 16:43:56 -0500

    edited the css

--- controllers/home.php
+++ controllers/home.php
@@ -13,12 +13,12 @@ $newAds = Ad::showNewest();
 
 			<div class="col-md-3">
 				<section class="sidebar col-md-10 col-md-offset-1">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex earum nisi repellat dicta vitae aspernatur quod quis error corrupti, corporis blanditiis cum excepturi voluptate qui odit rerum inventore eveniet sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi pariatur veniam similique officiis aliquam, voluptate ducimus, officia error provident, quia fugiat et reprehenderit earum itaque. Deleniti necessitatibus inventore molestiae sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quasi necessitatibus voluptatem, adipisci, fuga expedita atque dolore. Veniam dolore, omnis, sit expedita sint hic neque ea quae, quia ullam dolores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quas molestias id ipsam tempora, magnam vero nesciunt qui delectus dolorum incidunt dolor officiis dicta, ipsum harum! Quod dolorum, ipsum pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iusto facere alias ut magni dicta aut quo, officia minus esse facilis unde? Eum quasi blanditiis reiciendis, aliquid quas, porro eius.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex earum nisi repellat dicta vitae aspernatur quod quis error corrupti, corporis blanditiis cum excepturi voluptate qui odit rerum inventore eveniet sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi pariatur veniam similique officiis aliquam, voluptate ducimus, officia error provident, quia fugiat et reprehenderit earum itaque. Deleniti necessitatibus inventore molestiae sapiente!</p>
 				</section> <!-- .sidebar -->
 			</div>
 
 			<section class="col-md-8 missionStatement">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex earum nisi repellat dicta vitae aspernatur quod quis error corrupti, corporis blanditiis cum excepturi voluptate qui odit rerum inventore eveniet sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi pariatur veniam similique officiis aliquam, voluptate ducimus, officia error provident, quia fugiat et reprehenderit earum itaque. Deleniti necessitatibus inventore molestiae sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quasi necessitatibus voluptatem, adipisci, fuga expedita atque dolore. Veniam dolore, omnis, sit expedita sint hic neque ea quae, quia ullam dolores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quas molestias id ipsam tempora, magnam vero nesciunt qui delectus dolorum incidunt dolor officiis dicta, ipsum harum! Quod dolorum, ipsum pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iusto facere alias ut magni dicta aut quo, officia minus esse facilis unde? Eum quasi blanditiis reiciendis, aliquid quas, porro eius.</p>
+				<p>Are you sick of browsing craiglist for endless hours, or going to the mall just to walk out empty handed as they did not have the deck that you wanted???? Well now all that will change, we have endless amounts of skate gear for trade/sale that people around you are trying to get rid of. Come browse our site and find something you might like.</p>
 			</section>
 
 		</div> <!-- .row -->
@@ -31,20 +31,23 @@ $newAds = Ad::showNewest();
 
 		<!-- <div class="row"> -->
 			<!-- <div class="col-md-8 col-md-2 offset"> - -->
-			<div class="row">
-				<div class="col-md-4"></div>
-				<div class="col-md-8">
-					<?php foreach($newAds as $key => $newAd): ?>
-						<div class="individualPictureHome col-md-6">
-							<a href="/ads/info?ad=<?=$newAd['id'];?>"><img class="img-thumbnail indexPicture" src="<?=$newAd['image'];?>" alt="test"></a>
-							<p id="imageTitle"><?=$newAd['title'];?></p>
-						</div>
-					<?php endforeach; ?>
-				</div> <!-- .col-md-8 -->
-			</div>
 
 
 
+		<div class="row text-center">
+
+				   <?php foreach($newAds as $key => $newAd): ?>
+					   <div class="col-md-6 col-sm-12 hero-feature">
+						   <div class="thumbnail">
+ 						  		<img class="img-thumbnail indexPicture" src="<?=$newAd['image'];?>" alt="test">
+ 						  		<div class="caption">
+ 						  			<p id="imageTitle"><?=$newAd['title'];?></p>
+									<p class="btn btn-primary"><a href="/ads/info?ad=<?=$newAd['id'];?>">See More</a>
+ 					  			</div>
+				  		</div>
+			  		</div>
+ 				  <?php endforeach; ?>
+		   </div>
 
 
 <?php require_once '../views/partials/footer.php'; ?>
